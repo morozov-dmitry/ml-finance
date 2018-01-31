c8cafa22d7df11014fe21d1a29f448cb105cf1ec
Dmitry Morozov <Morosovdmitry@gmail.com>
2018-01-31 10:21:41 +0200

    Implementing action for polling data from Yahoo Finance and saving it to MongoDB.

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,71 @@
 let express = require('express');
-let app = express();
+let dateformat = require('dateformat');
 let yahooFinance = require('yahoo-finance');
+let app = express();
+
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://ucp-mongodb:27017/";
+
+// Stock prices symbols to download
+let symbols = ['GOOG', 'IBM', 'AAPL', 'NVDA', 'SPY']
+
+let createSymbolsLoadHash = () => {
+    let loadHash = {}
+    symbols.forEach((symbol) => {
+        loadHash[symbol] = false
+    })
+    return loadHash;
+}
 
 app.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ 'message' : 'Welcome to udacity-capstone-finance', 'version' : '0.0.1' }));
+    res.send('Hello World!');
 });
 
-app.get('/get-data', function (req, res) {
+app.get('/load', function (req, res) {
 
-    yahooFinance.historical({
-        symbol: 'AAPL',
-        from: '2012-01-01',
-        to: '2012-12-31',
-        // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-    }, function (err, quotes) {
-        console.log(quotes);
-        res.send(quotes);
-    });
+    // Current day
+    const dateTo = new Date;
+    // Previous day
+    const dateFrom = new Date(dateTo.getTime() - 24 * 60 * 60 * 1000);
 
-    res.send('Welcome to udacity-capstone-finance v.0.0.1');
+    let loadHash = createSymbolsLoadHash()
+
+    const processSymbolData = (err, quotes, symbol) => {
+
+        loadHash[symbol] = true
+        let showResponse = true
+        for(let hashSymbol in loadHash){
+            if(!loadHash[hashSymbol]){
+                showResponse = false
+                break
+            }
+        }
+
+        if(showResponse){
+            res.send('Data downloaded');
+        }
+
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("udacity-finance");
+            db.collection("stock_log").insertMany(quotes, function(err, res) {
+                if (err) throw err;
+                console.log("1 documents were inserted");
+                db.close();
+            });
+        });
+
+    }
+
+    for(let symbol in loadHash){
+        yahooFinance.historical({
+            symbol: symbol,
+            from: dateformat(dateFrom, "yyyy-mm-dd"),
+            to: dateformat(dateTo, "yyyy-mm-dd"),
+        }, (err, quotes) => processSymbolData(err, quotes, symbol));
+    }
+
+
 });
 
 app.listen(3000, function () {
