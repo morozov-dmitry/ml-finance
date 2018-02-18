f9f4ef76efd2090fab46e9235032eeae1889168d
Dmitry Morozov <Morosovdmitry@gmail.com>
2018-02-18 09:01:45 +0200

    Adding ml files

---
+++ src/ml/capstone.py
@@ -0,0 +1,37 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score
+
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between
+        true and predicted values based on the metric chosen. """
+
+    score = r2_score(y_true, y_predict)
+
+    # Return the score
+    return score
+
+# Read student data
+financial_data = pd.read_csv("financial_data.csv")
+print "Financial data read successfully!"
+
+prices = financial_data['Adj Close']
+features = financial_data.drop('Adj Close', axis = 1)
+
+minimum_price = np.min(prices)
+maximum_price = np.max(prices)
+mean_price = np.mean(prices)
+median_price = np.median(prices)
+std_price = np.std(prices)
+
+# Show the calculated statistics
+print "Statistics for Boston housing dataset:\n"
+print "Minimum price: ${:,.2f}".format(minimum_price)
+print "Maximum price: ${:,.2f}".format(maximum_price)
+print "Mean price: ${:,.2f}".format(mean_price)
+print "Median price ${:,.2f}".format(median_price)
+print "Standard deviation of prices: ${:,.2f}".format(std_price)
+
+from sklearn.ensemble import RandomForestRegressor
