aac19470c900c3af26c736bb715ccde24960d770
Morozov Dmitry <morozov@zfort.com>
2018-03-29 19:21:27 +0300

    Playing around with different models.

--- src/ml/app.py
+++ src/ml/app.py
@@ -4,6 +4,10 @@ from datetime import datetime, date, timedelta
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import cross_validation
+
 from pymongo import MongoClient
 import pickle
 import logging
@@ -35,27 +39,64 @@ def predict():
 
     # Define time frame for historical data to train the model
     today = datetime.today()
-    start_date = today - timedelta(days=31)
+    start_date = today - timedelta(days=60)
+
+    prediction_dates = []
+
+    # Timeseries for predict
+    numdays = 7
+    for future_day_number in range(0, numdays):
+        future_date = today + timedelta(days=future_day_number)
+        prediction_dates.append([future_date.toordinal()])
 
     # Load historical data from database
     historical_data = {}
     for stock_data in collection.find({"date": {"$gt": start_date}}, {"date": 1, "adjClose": 1, "symbol": 1, "_id": 0}):
         if stock_data['symbol'] not in historical_data:
             historical_data[stock_data['symbol']] = {'X':[], 'Y':[]}
-        historical_data[stock_data['symbol']]['X'].append(stock_data['date'].strftime("%Y-%m-%d %H:%M:%S"))
-        historical_data[stock_data['symbol']]['Y'].append(stock_data['adjClose'])
-
-    financial_data = pd.read_csv("financial_data_small.csv")
-    logging.info(financial_data)
+        historical_data[stock_data['symbol']]['X'].append([stock_data['date'].toordinal()])
+        historical_data[stock_data['symbol']]['Y'].append(np.log(stock_data['adjClose']))
 
     # Train models
+    predicted_data = {}
     for symbol in historical_data:
+
+        symbol_predicted_data = {'KNeighborsRegressor': {}, 'RandomForestRegressor': {}, 'LinearRegression': {}}
         symbol_stock_data = historical_data[symbol]
-        logging.info(symbol_stock_data)
+
+        logging.info("symbol_stock_data['X']")
+        logging.info(symbol_stock_data['X'])
+        logging.info("prediction_dates")
+        logging.info(prediction_dates)
+
+        X_train, X_test, y_train, y_test = cross_validation.train_test_split(symbol_stock_data['X'], symbol_stock_data['Y'], test_size=0.2, train_size=0.8, random_state=3)
+
+        # KNeighborsRegressor model
         reg_model = KNeighborsRegressor(n_neighbors=2)
-        reg_model.fit(np.asarray([symbol_stock_data['X']]), np.asarray([symbol_stock_data['Y']]))
+        reg_model.fit(X_train, y_train)
+        predicted_prices = reg_model.predict(prediction_dates)
+        for i in symbol_predicted_data:
+            symbol_predicted_data['KNeighborsRegressor'] = predicted_prices
 
+        # RandomForestRegressor model
+        reg_model = RandomForestRegressor(random_state=0)
+        reg_model.fit(symbol_stock_data['X'], symbol_stock_data['Y'])
+        predicted_prices = reg_model.predict(prediction_dates)
+        for i in symbol_predicted_data:
+            symbol_predicted_data['RandomForestRegressor'] = predicted_prices
 
+        # LinearRegression model
+        reg_model = LinearRegression()
+        reg_model.fit(symbol_stock_data['X'], symbol_stock_data['Y'])
+        predicted_prices = reg_model.predict(prediction_dates)
+        for i in symbol_predicted_data:
+            symbol_predicted_data['LinearRegression'] = predicted_prices
+
+        logging.info(predicted_prices)
+        predicted_data[symbol] = symbol_predicted_data;
+        # logging.info(predicted_prices)
+
+    logging.info(predicted_data)
     logging.info('Finished')
 
     # Sending response with message
