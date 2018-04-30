ad8dc1b7105916768fbda7f3173ec503cb65a5ec
Dmitry Morozov <Morosovdmitry@gmail.com>
2018-04-30 14:28:21 +0300

    app fix for python

--- src/ml/app.py
+++ src/ml/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 import click
-from flask import Flask, Response, json
+from flask import Flask
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from utils.prediction_helper import PredictionHelper
@@ -8,19 +8,17 @@ from utils.prediction_helper import PredictionHelper
 
 app = Flask(__name__)
 
-cc
-# @app.cli.command()
+@app.cli.command()
 def hello():
     """Initialize the database."""
     click.echo('Hello CLI')
 
-c
 @app.cli.command()
 def predict():
 
     # Define time frame for historical data to train the model
     today = datetime.today()
-    start_date = today - timedelta(days = 60)
+    start_date = today - timedelta(days = 30)
 
     prediction_helper = PredictionHelper()
     prediction_helper.make_prediction(start_date)
@@ -28,10 +26,7 @@ def predict():
     # print results
     click.echo('Machine learning algorithms has been run. Predicted data saved to database')
 
-    #return prepare_response()c
 
-
-c
 @app.cli.command()
 def predict_historical():
 
@@ -44,19 +39,6 @@ def predict_historical():
 
     # print results
     click.echo('Machine learning algorithms has been run. Predicted data saved to database')
-
-    # Response
-    # return prepare_response()
-
-
-def prepare_response():
-    data = {'status': 0, 'data': "Machine learning algorithms has been run. Predicted data saved to database"}
-    response = Response(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
 
 
 
